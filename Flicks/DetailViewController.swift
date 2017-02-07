58bde1180ecb91327c4a29cd156ae9259dfc9a8d
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2017-02-07 16:00:21 -0500

    Added poster to the detail view

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -20,11 +20,42 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Title
         let title = movie["title"] as? String
         titleLabel.text = title
         
+        // Overview
         let overview = movie["overview"] as? String
         overviewLabel.text = overview
+        
+        
+        // Poster Image
+        let baseURL = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        let imageURL = NSURL(string: baseURL + posterPath)
+        let imageRequest = NSURLRequest(url: imageURL as! URL)
+        
+        posterImageView.setImageWith(
+            imageRequest as URLRequest,
+            placeholderImage: nil,
+            success: { (imageRequest, imageResponse, image) -> Void in
+                
+                // imageResponse will be nil if the image is cached
+                if imageResponse != nil {
+                    print("Image was NOT cached, fade in image")
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = image
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in self.posterImageView.alpha = 1.0
+                    })
+                } else {
+                    print("Image was cached so just update the image")
+                    self.posterImageView.image = image
+                }
+        },
+            failure: { (imageRequest, imageResponse, error) -> Void in
+                // do something for the failure condition
+        })
+        
         
         print (movie)
 
