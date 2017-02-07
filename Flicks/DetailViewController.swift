16a2b4f782bc864b690fdcc941edee033dc69ccd
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2017-02-07 17:20:28 -0500

    Added poster in the detail view & made detail view scrollable

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import Cosmos
 
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var viewInfo: UIView!
+    @IBOutlet weak var cosmosView: CosmosView!
     
     var movie: NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: viewInfo.frame.origin.y + viewInfo.frame.size.height)
         
         // Title
         let title = movie["title"] as? String
@@ -28,12 +33,40 @@ class DetailViewController: UIViewController {
         let overview = movie["overview"] as? String
         overviewLabel.text = overview
         
+        overviewLabel.sizeToFit() // grow label with text size
+        
         
         // Poster Image
         let baseURL = "https://image.tmdb.org/t/p/w500"
         let posterPath = movie["poster_path"] as! String
         let imageURL = NSURL(string: baseURL + posterPath)
         let imageRequest = NSURLRequest(url: imageURL as! URL)
+        
+        
+        // Star Rating
+        
+        let star = (movie["vote_average"] as! Float) / 2
+        
+        // show stars precisely, not only full stars
+        cosmosView.settings.fillMode = .precise
+        
+        // Change the cosmos view rating
+        cosmosView.rating = Double(star)
+        
+        
+        // Change the text
+        cosmosView.text = " " + String(star)
+        
+        
+        // Called when user finishes changing the rating by lifting the finger from the view.
+        // This may be a good place to save the rating in the database or send to the server.
+        cosmosView.didFinishTouchingCosmos = { rating in }
+        
+        // A closure that is called when user changes the rating by touching the view.
+        // This can be used to update UI as the rating is being changed by moving a finger.
+        cosmosView.didTouchCosmos = { rating in }
+        
+        
         
         posterImageView.setImageWith(
             imageRequest as URLRequest,
